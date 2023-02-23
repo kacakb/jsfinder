7c9d6971be39c846084b34234868d41578d4a06c
k4Ck <64865400+kacakb@users.noreply.github.com>
2023-02-23 19:49:40 +0300

    Update jsfinder.go

--- jsfinder.go
+++ jsfinder.go
@@ -5,82 +5,104 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"path/filepath"
 	"regexp"
-	"time"
-	"os"
 	"sync"
+	"time"
 )
 
 func main() {
-
-	urlsFile, err := os.Open("url.txt") 
+	// Open URLs file
+	urlsFile, err := os.Open("urllist.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer urlsFile.Close()
 
+	// Channel to collect results from goroutines
 	results := make(chan string)
 
+	// Semaphore to limit number of HTTP requests made at once
 	sem := make(chan struct{}, 50)
 
+	// Wait group to wait for all goroutines to finish
 	var wg sync.WaitGroup
 
-}
+	// Read URLs file line by line
+	scanner := bufio.NewScanner(urlsFile)
+	for scanner.Scan() {
+		url := scanner.Text()
 
-scanner :=bufio.NewScanner(urlsFile)
-for scanner.Scan(){
-	url:=scanner.Text()
-	wg.Add(1)
+		wg.Add(1)
 
-	go func (url string) {
+		go func(url string) {
+			defer wg.Done()
 
-		defer wg.Done()
-		sem <- struct{}{}
-		defer func(){<-sem}()
+			// Acquire semaphore
+			sem <- struct{}{}
+			defer func() { <-sem }()
 
-		client:= &http.Client{
-			timeout: 5 * time.Second,
-		}
-		req, err := http.NewRequest("GET", url,nil)
-		if err != nil {
-			fmt.println("Error creating request for %s: %s", url, err)
-			return
+			// Create HTTP client with custom User-Agent header and timeout
+			client := &http.Client{
+				Timeout: 5 * time.Second,
+			}
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				fmt.Printf("Error creating request for %s: %v\n", url, err)
+				return
+			}
+			req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36")
 
-}
-req.Header.Set("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.43")
-resp ,err := client.Do(req)
-if err := nil {
-	fmt.Printf("Error getting response from %s: %v\n", url, err)
-	return
-}
-defer resp.Body.Close()
+			// Send GET request to URL
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Printf("Error getting response from %s: %v\n", url, err)
+				return
+			}
+			defer resp.Body.Close()
 
-if resp.statusCode := http.StatusOK{
+			// Check status code for successful request
+			if resp.StatusCode != http.StatusOK {
+				fmt.Printf("Error getting response from %s: status code %d\n", url, resp.StatusCode)
+				return
+			}
 
-	fmt.Printf("Error getting response from %s: %v\n", url, resp.statusCode)
-	return
-}
-defer file.Close(
+			// Read response body
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Printf("Error reading response from %s: %v\n", url, err)
+				return
+			}
+			bodyString := string(bodyBytes)
 
-	for_, match := range matches {
-		jsURL :=match[1]
-		if filepath.Ext(jsURL) == ".js" {
-			fullURL := url + "/" + jsURL
-			file.WriteString(fmt.Sprintf("%s\n", fullURL))
-		}
+			// Find JavaScript files using regular expression
+			re := regexp.MustCompile(`src="([^"]+\.js)"`)
+
+
+
+			matches := re.FindAllStringSubmatch(bodyString, -1)
+			if len(matches) > 0 {
+				// Write JavaScript file names to file
+				file, err := os.OpenFile("found.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+				if err != nil {
+					fmt.Printf("Error opening file for writing: %v\n", err)
+					return
+				}
+				defer file.Close()
+
+				for _, match := range matches {
+					jsURL := match[1]
+					if filepath.Ext(jsURL) == ".js" {
+						fullURL := url + "/" + jsURL
+						file.WriteString(fmt.Sprintf("%s\n", fullURL))
+					}
+				}
+			}
+		}(url)
 	}
-	}
-	}(url)
+
+	// Wait for all goroutines to complete and print results
+	wg.Wait()
+	close(results)
 }
-
-wg.Wait()
-close(results)
-
-}
-
-
-
-
-
-
