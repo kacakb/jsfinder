3e85399d2c975371a3038d06465ccd3915906490
k4Ck <64865400+kacakb@users.noreply.github.com>
2023-02-24 11:24:51 +0300

    Finalz

--- jsfinder.go
+++ jsfinder.go
@@ -145,8 +145,11 @@ func main() {
 
 				for _, match := range matches {
 					jsURL := match[1]
-					if strings.HasSuffix(jsURL, ".js") {
+					if strings.Contains(jsURL, ".js") && strings.Contains(jsURL, ".com") && !strings.HasPrefix(jsURL, "https://") {
+						file.WriteString(fmt.Sprintf("https://%s\n", jsURL))
+					} else if strings.HasSuffix(jsURL, ".js") {
 						if strings.HasPrefix(jsURL, "/") {
+							jsURL = strings.TrimPrefix(jsURL, "/")
 							file.WriteString(fmt.Sprintf("%s%s\n", url, jsURL))
 						} else if strings.HasPrefix(jsURL, "https://") {
 							file.WriteString(fmt.Sprintf("%s\n", jsURL))
